103dd2568066d27b087083790c14ede729d77c06
Deb Johnson <1.deborah.r.johnson@gmail.com>
2025-07-11 21:21:41 -0500

    Add profile screen

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,22 @@
+import { Button, View, StyleSheet } from "react-native";
+import { theme } from "@/theme";
+import { useUserStore } from "@/store/userStore";
+
+export default function ProfileScreen() {
+  const toggleHasOnboarded = useUserStore((state) => state.toggleHadOnboarded);
+
+  return (
+    <View style={styles.container}>
+      <Button title="Back to onboarding" onPress={toggleHasOnboarded} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: theme.colorWhite,
+  },
+});
