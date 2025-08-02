9cea0c659a963423cf5532988cf8d56b67dfefaa
Deb Johnson <1.deborah.r.johnson@gmail.com>
2025-08-01 20:11:12 -0500

    Add plants store

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { theme } from "@/theme";
+import { PlantCard } from "@/components/PlantCard";
+import { usePlantStore } from "@/store/plantsStore";
+
 export default function App() {
+  const plants = usePlantStore((state) => state.plants);
+
+  return (
+    <FlatList
+      data={plants}
+      renderItem={({ item }) => <PlantCard plant={item} />}
+    />
+  );
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
